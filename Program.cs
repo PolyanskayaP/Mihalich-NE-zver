095d775fbca3dbfce25d5455448778f05e72ffb2
Полина Полянская <ppolina353@yandex.ru>
2021-09-12 21:12:57 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mihalich_NE_zver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
